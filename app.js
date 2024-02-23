3ea03625a370710f54e653b702570eb9a878ab70
Justin Bossis <bossisjustin@gmail.com>
2024-02-23 13:55:14 +0100

    Init project

---
+++ app.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require("express");
+const morgan = require('morgan');
+const app = express()
+require("dotenv").config()
+const port = process.env.PORT;
+
+app.use(morgan('tiny'));
+app.use(express.static("public"));
+app.set('views', path.join(__dirname, "views"));
+app.set('view engine', 'pug');
+app.use(express.urlencoded({ extended: true }));
+
+
+const indexRouter = require("./routes/index.js");
+
+app.use("/", indexRouter);
+
+app.use( (req, res, next) => {
+  res.status(404).send('Page not found!');
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+  console.log(process.env.NODE_ENV)
+});
