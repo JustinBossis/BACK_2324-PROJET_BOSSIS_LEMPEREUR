f4d5fcef434c1ac5193a28bcbf33afcacbb02b54
Noemie-Lempereur <lempereur.noemie@gmail.com>
2024-03-15 11:37:11 +0100

    add refreshToken

--- routes/user.js
+++ routes/user.js
@@ -2,14 +2,6 @@ const express = require('express');
 const user = require("../model/User");
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    user.getById("65d8a7b0080138bf9f04bab4").then((event) => {
-        res.send(event);
-    }).catch(() => {
-        res.status(404).send('Page not found!');
-    })
-});
-
 router.get('/:userId', async (req, res) => {
     user.getById(req.params.userId).then((event) => {
         res.send(event);
@@ -18,8 +10,8 @@ router.get('/:userId', async (req, res) => {
     })
 });
 
-router.get('/connect/:emailUser', async (req, res) => {
-    user.connectUser(req.params.emailUser, "tressecurise").then((event) => {
+router.get('/connect/', async (req, res) => {
+    user.connectUser(req.query.email, req.query.password).then((event) => {
         res.send(event);
     }).catch(() => {
         res.status(404).send('Page not found!');
@@ -28,10 +20,10 @@ router.get('/connect/:emailUser', async (req, res) => {
 
 router.post('/',async (req, res, next) => {
     try {
-        const data= {
+        /*const data= {
             firstname: "testfirst", lastname: "testlast", username:"usertest", email: "testemail@test.com",password: "tressecurise",admin:"false", favorites: [], birthdate:"01/12/2000", picture:"public/images/users/avatar.png"
-        }
-        res.send (await user.createUser(data));
+        }*/
+        res.send (await user.createUser(req.body));
     } catch (e) {
         console.error("Erreur : ", e);
     }
@@ -39,7 +31,7 @@ router.post('/',async (req, res, next) => {
 
 router.post('/refreshtoken',(req, res, next) => {
     try {
-        res.send(user.refreshToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZjQwY2UyMmI5NjlkNTM3MTQxMGJmZiIsImlhdCI6MTcxMDQ5ODQ2OCwiZXhwIjoxNzEwNTg0ODY4fQ.R-Qx1bv6AAZAVMNfyFYxk9Fd0ldfzG-WHdPPA2m7VK4"));
+        res.send(user.refreshToken(req.body.refreshToken));
     } catch (e) {
         console.error("Erreur : ", e);
         res.send(e);
