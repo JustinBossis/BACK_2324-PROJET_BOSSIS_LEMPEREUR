b6cfa3519e8213bf17d0ede7976fe67eb876d47c
Noemie-Lempereur <lempereur.noemie@gmail.com>
2024-03-15 10:31:13 +0100

    add user

--- model/User.js
+++ model/User.js
@@ -1,29 +1,50 @@
 const { MongoClient, ObjectId} = require('mongodb');
+const bcrypt = require("bcryptjs")
 
 const url = process.env.MONGODB_URI;
 const dbName = 'projet';
 const client = new MongoClient(url);
 
+
 let modelUser = {_id: "1", firstname: "firstname", lastname: "lastname", username:"username", email: "test@test.com",password: "test",admin:"false", favorites: [], birthdate:"01/12/2000", picture:"public/images/users/avatar.png", };
+
+
 
 const User = {
 
     //Connexion utilisateur
     //email password
-    connexionUser: async function (email, password) {
+    connectUser: async function (email, password) {
         try {
             await client.connect();
             const db = client.db(dbName);
             const usersCollection = db.collection('users');
             const user = await usersCollection.findOne({email: email});
             if (!user) {
+                console.log("pas de user trouvé avec l'adresse mail");
                 return null;
             }
             //verifier si le password correspond
-            if (!password === user.password) {
-                return null;
+            try {
+                // Comparaison du mot de passe
+                return new Promise((resolve, reject) => {
+                    bcrypt.compare(password, user.password, function (err, res) {
+                        if (err) {
+                            console.log(err);
+                            reject(err);
+                        }
+                        if (res) {
+                            // JWT
+                            resolve(user);
+                        } else {
+                            console.log("Le mot de passe ne correspond pas");
+                            reject(new Error('Le mot de passe ne correspond pas'));
+                        }
+                    });
+                });
+            } catch (error) {
+                throw error;
             }
-            return user;
         } catch (error) {
             throw error;
         } finally {
@@ -38,6 +59,8 @@ const User = {
             await client.connect();
             const db = client.db(dbName);
             const usersCollection = db.collection('users');
+            const salt = bcrypt.genSaltSync(10);
+            data.password = bcrypt.hashSync(data.password, salt);
             const newUser = {
                 ...data
             };
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,38 @@ const user = require("../model/User");
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    console.log(await user.getById("65d8a7b0080138bf9f04bab4"));
+    user.getById("65d8a7b0080138bf9f04bab4").then((event) => {
+        res.send(event);
+    }).catch(() => {
+        res.status(404).send('Page not found!');
+    })
+});
+
+router.get('/:userId', async (req, res) => {
+    user.getById(req.params.userId).then((event) => {
+        res.send(event);
+    }).catch(() => {
+        res.status(404).send('Page not found!');
+    })
+});
+
+router.get('/connect/:emailUser', async (req, res) => {
+    user.connectUser(req.params.emailUser, "tressecuriseMaismauvais").then((event) => {
+        res.send(event);
+    }).catch(() => {
+        res.status(404).send('Page not found!');
+    })
+});
+
+router.post('/',async (req, res, next) => {
+    try {
+        const data= {
+            firstname: "testfirst", lastname: "testlast", username:"usertest", email: "testemail@test.com",password: "tressecurise",admin:"false", favorites: [], birthdate:"01/12/2000", picture:"public/images/users/avatar.png"
+        }
+        await user.createUser(data);
+    } catch (e) {
+        console.error("Erreur : ", e);
+    }
 });
 
 module.exports = router;
