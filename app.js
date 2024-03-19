4c7f78ccc824db04e105ebad8dc1d45cd84c4182
Justin Bossis <bossisjustin@gmail.com>
2024-03-19 13:50:25 +0100

    Use express json

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(morgan('tiny'));
 app.use(express.static("public"));
 app.set('views', path.join(__dirname, "views"));
 app.set('view engine', 'pug');
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 
